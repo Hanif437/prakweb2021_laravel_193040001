1953d4c92aa36fc39c502df422087fafd31919de
Hanif437 <Putrahanif437@gmail.com>
2021-10-07 16:52:51 +0700

    memodifikasi file web.php pada video ke 3 di pertemuan ke 5

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Putra Hanif Ammarta",
+        "email" => "putrahanif437@gmail.com",
+        "image" => "profile.jfif"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
