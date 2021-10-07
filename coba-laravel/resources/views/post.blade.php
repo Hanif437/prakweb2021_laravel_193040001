ebb9febe3b6dbcfe92dcb2c5ea54ffc2504bbe84
Hanif437 <Putrahanif437@gmail.com>
2021-10-07 19:53:59 +0700

    memodifikasi file post.blade.php pada video ke 4 di pertemuan ke 5

---
+++ coba-laravel/resources/views/post.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('container')
+
+    <article>
+        <h2>{{ $post["title"] }}</h2>
+        <h4>{{ $post["author"] }}</h4>
+        <p>{{ $post["body"] }}</p>
+    </article>
+
+    <a href="/blog">Back</a>
+
+@endsection
