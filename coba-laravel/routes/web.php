bc763712c0dd9934ec090f68648a21144dc5c130
Hanif437 <Putrahanif437@gmail.com>
2021-10-14 01:30:09 +0700

    memodifikasi file web.php pada video ke 1 di pertemuan ke 6

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -30,59 +33,5 @@ Route::get('/about', function () {
 
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Putra Hanif Ammarta",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, quibusdam, maiores officiis suscipit quod esse aperiam assumenda temporibus pariatur accusantium quam veniam repellendus nostrum quia dolorum consequuntur numquam quas eaque amet dolor quidem, consectetur provident! Quod consequuntur quo suscipit perferendis cupiditate a possimus, eius reiciendis nesciunt dignissimos quam laudantium? Quidem, voluptatem ea? Numquam, cum provident accusamus assumenda maiores, ab delectus adipisci incidunt odit minus veritatis reprehenderit iure expedita aliquid impedit nesciunt earum eos facilis facere sunt natus quod! Molestias, velit."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Putra Hanif Ammarta",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere quisquam obcaecati doloremque consequatur laboriosam, error voluptatum perspiciatis totam nam. Quasi minima consectetur praesentium, fuga pariatur soluta, dolores aspernatur, unde architecto veniam ducimus non fugit libero porro placeat obcaecati aperiam id culpa similique cupiditate eveniet blanditiis. Provident dolores necessitatibus accusantium laboriosam omnis cumque numquam deleniti iste eum? Corrupti ullam labore ad ratione totam sint officia architecto non accusamus dolorum, provident neque. Quo deleniti voluptates temporibus error autem ipsam omnis illo voluptatibus itaque modi quod, necessitatibus labore rerum? Explicabo laudantium iure excepturi, enim laborum voluptate? Eaque, ipsum voluptate. Voluptates dolorum vero placeat."
-        ]
-        ];
-
-    return view('posts', [
-        "title" => "Posts",
-        "posts" =>  $blog_posts
-    ]);
-});
-
-
-
-// Halaman single post
-
-Route::get('posts/{slug}', function($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Putra Hanif Ammarta",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, quibusdam, maiores officiis suscipit quod esse aperiam assumenda temporibus pariatur accusantium quam veniam repellendus nostrum quia dolorum consequuntur numquam quas eaque amet dolor quidem, consectetur provident! Quod consequuntur quo suscipit perferendis cupiditate a possimus, eius reiciendis nesciunt dignissimos quam laudantium? Quidem, voluptatem ea? Numquam, cum provident accusamus assumenda maiores, ab delectus adipisci incidunt odit minus veritatis reprehenderit iure expedita aliquid impedit nesciunt earum eos facilis facere sunt natus quod! Molestias, velit."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Putra Hanif Ammarta",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere quisquam obcaecati doloremque consequatur laboriosam, error voluptatum perspiciatis totam nam. Quasi minima consectetur praesentium, fuga pariatur soluta, dolores aspernatur, unde architecto veniam ducimus non fugit libero porro placeat obcaecati aperiam id culpa similique cupiditate eveniet blanditiis. Provident dolores necessitatibus accusantium laboriosam omnis cumque numquam deleniti iste eum? Corrupti ullam labore ad ratione totam sint officia architecto non accusamus dolorum, provident neque. Quo deleniti voluptates temporibus error autem ipsam omnis illo voluptatibus itaque modi quod, necessitatibus labore rerum? Explicabo laudantium iure excepturi, enim laborum voluptate? Eaque, ipsum voluptate. Voluptates dolorum vero placeat."
-        ]
-        ];
-
-
-        $new_post = [];
-        foreach($blog_posts as $post){
-            if($post["slug"] === $slug){
-                $new_post = $post;
-            }
-        }
-
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index'] );
+Route::get('posts/{slug}', [PostController::class, 'show']);
