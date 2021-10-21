0376d5059e50477a796c3560a3a0bfe0cd7fff0c
Hanif437 <Putrahanif437@gmail.com>
2021-10-21 09:53:25 +0700

    memodifikasi file post.blade.php pada video ke 1 di pertemuan ke 7

--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -5,12 +5,12 @@
     <article>
         <h2 class="mb-5">{{ $post->title }}</h2>
 
-        <p>By. Putra Hanif Ammarta in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
         
         {!! $post->body !!}
 
     </article>
 
-    <a href="/blog">Back</a>
+    <a href="/blog" class="d-block text-decoration-none mt-3">Back</a>
 
 @endsection
