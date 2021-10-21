85ca38723312615aa9608a0228bc7d14e166bbb1
Hanif437 <Putrahanif437@gmail.com>
2021-10-21 13:48:55 +0700

    memodifikasi file web.php pada video ke 2 di pertemuan ke 7

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 use App\Models\Category;
+use App\Models\User;
 
 
 /*
@@ -52,5 +53,13 @@ Route::get('/categories/{category:slug}', function(Category $category)
         'title' => $category->name,
         'posts' => $category->posts,
         'category' => $category->name
+    ]);
+});
+
+Route::get('/authors/{author:username}', function(User $author){
+    return view('posts', 
+    [
+        'title' => 'User Post',
+        'posts' => $author->posts,
     ]);
 });
