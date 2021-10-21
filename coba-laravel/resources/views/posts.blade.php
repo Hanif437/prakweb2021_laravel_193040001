c2bd2a05f031334b394819960f05472f7b06505b
Hanif437 <Putrahanif437@gmail.com>
2021-10-21 09:53:40 +0700

    memodifikasi file posts.blade.php pada video ke 1 di pertemuan ke 7

--- coba-laravel/resources/views/posts.blade.php
+++ coba-laravel/resources/views/posts.blade.php
@@ -4,11 +4,16 @@
 
 
     @foreach($posts as $post)
-    <article class="mb-3">
+    <article class="mb-3 border-bottom pb-3">
     <h2>
-        <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+        <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
     </h2>
+
+    <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+
     <p>{{ $post->excerpt }}</p>
+
+    <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read More...</a>
     </article>
     @endforeach
 
