86f382690a929ff5e11ec7469c8bad46d21e0381
Hanif437 <Putrahanif437@gmail.com>
2021-10-21 16:28:49 +0700

    memodifikasi file post.blade.php pada video ke 4 di pertemuan ke 7

--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -2,15 +2,27 @@
 
 @section('container')
 
-    <article>
-        <h2 class="mb-5">{{ $post->title }}</h2>
-
-        <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-8">
+                <h2 class="mb-3">{{ $post->title }}</h2>
+                
         
-        {!! $post->body !!}
 
-    </article>
+                    <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+                    
+                    <img src="https://source.unsplash.com/800x400/?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="image-fluid">
+                    <article class="my-3 fs-5">
 
-    <a href="/blog" class="d-block text-decoration-none mt-3">Back</a>
+                        {!! $post->body !!}
+            
+                    </article>
+            
+                <a href="/blog" class="d-block text-decoration-none mt-3">Back</a>
+            </div>
+        </div>
+    </div>
+
+    
 
 @endsection
