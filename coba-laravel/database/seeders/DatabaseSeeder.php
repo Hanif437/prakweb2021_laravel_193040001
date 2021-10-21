ae7ec26cbb037b054e40ab824e056c16c53fe491
Hanif437 <Putrahanif437@gmail.com>
2021-10-21 09:53:09 +0700

    memodifikasi file DatabaseSeeder.php pada video ke 1 di pertemuan ke 7

--- coba-laravel/database/seeders/DatabaseSeeder.php
+++ coba-laravel/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use \App\Models\User;
+use \App\Models\Category;
+use \App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,65 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Putra Hanif Ammarta',
+            'email' => 'putrahanif437@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Hanifuu Ameuta',
+            'email' => 'hanifuuameuta@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+        
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi libero unde, at voluptates minus ipsum officiis beatae praesentium, non inventore error doloribus commodi quasi quis saepe velit voluptas exercitationem maxime fugit? Quas officiis porro magni adipisci culpa odit labore aliquid necessitatibus quidem nam ducimus commodi, quibusdam voluptates pariatur fuga aut iusto qui, omnis perspiciatis facilis atque optio. Corporis debitis recusandae quaerat nostrum repudiandae. Placeat ad adipisci explicabo provident veritatis! Quo sequi corrupti nemo unde quia earum dolorum dolore facere, iusto deleniti non ad totam sit adipisci commodi. Vel labore, reprehenderit quidem eos culpa nostrum sit aliquam, odit quas id quos.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi libero unde, at voluptates minus ipsum officiis beatae praesentium, non inventore error doloribus commodi quasi quis saepe velit voluptas exercitationem maxime fugit? Quas officiis porro magni adipisci culpa odit labore aliquid necessitatibus quidem nam ducimus commodi, quibusdam voluptates pariatur fuga aut iusto qui, omnis perspiciatis facilis atque optio. Corporis debitis recusandae quaerat nostrum repudiandae. Placeat ad adipisci explicabo provident veritatis! Quo sequi corrupti nemo unde quia earum dolorum dolore facere, iusto deleniti non ad totam sit adipisci commodi. Vel labore, reprehenderit quidem eos culpa nostrum sit aliquam, odit quas id quos.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi libero unde, at voluptates minus ipsum officiis beatae praesentium, non inventore error doloribus commodi quasi quis saepe velit voluptas exercitationem maxime fugit? Quas officiis porro magni adipisci culpa odit labore aliquid necessitatibus quidem nam ducimus commodi, quibusdam voluptates pariatur fuga aut iusto qui, omnis perspiciatis facilis atque optio. Corporis debitis recusandae quaerat nostrum repudiandae. Placeat ad adipisci explicabo provident veritatis! Quo sequi corrupti nemo unde quia earum dolorum dolore facere, iusto deleniti non ad totam sit adipisci commodi. Vel labore, reprehenderit quidem eos culpa nostrum sit aliquam, odit quas id quos.',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-keempat',
+            'excerpt' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi libero unde, at voluptates minus ipsum officiis beatae praesentium, non inventore error doloribus commodi quasi quis saepe velit voluptas exercitationem maxime fugit? Quas officiis porro magni adipisci culpa odit labore aliquid necessitatibus quidem nam ducimus commodi, quibusdam voluptates pariatur fuga aut iusto qui, omnis perspiciatis facilis atque optio. Corporis debitis recusandae quaerat nostrum repudiandae. Placeat ad adipisci explicabo provident veritatis! Quo sequi corrupti nemo unde quia earum dolorum dolore facere, iusto deleniti non ad totam sit adipisci commodi. Vel labore, reprehenderit quidem eos culpa nostrum sit aliquam, odit quas id quos.',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
+
+        
     }
 }
