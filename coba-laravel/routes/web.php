07f6a148d3df3e9c2ea51989131e01e733eb0b0d
Hanif437 <Putrahanif437@gmail.com>
2021-10-28 20:30:13 +0700

    memodifikasi file web.php pada video ke 1 di pertemuan ke 8

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -20,13 +20,15 @@ use App\Models\User;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => "home"
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
+        "active" => "about",
         "name" => "Putra Hanif Ammarta",
         "email" => "putrahanif437@gmail.com",
         "image" => "profile.jfif"
@@ -35,7 +37,7 @@ Route::get('/about', function () {
 
 
 
-Route::get('/blog', [PostController::class, 'index'] );
+Route::get('/posts', [PostController::class, 'index'] );
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('/categories', function(){
@@ -44,23 +46,5 @@ Route::get('/categories', function(){
         'title' => 'Post Categories',
         'active' => 'categories',
         'categories' => Category::all()
-    ]);
-});
-
-Route::get('/categories/{category:slug}', function(Category $category)
-{
-    return view('posts', 
-    [
-        'title' => "Post by Category : $category->name",
-        'active' => 'categories',
-        'posts' => $category->posts->load('category', 'author')
-    ]);
-});
-
-Route::get('/authors/{author:username}', function(User $author){
-    return view('posts', 
-    [
-        'title' => "Post by Author : $author->name",
-        'posts' => $author->posts->load('category', 'author')
     ]);
 });
