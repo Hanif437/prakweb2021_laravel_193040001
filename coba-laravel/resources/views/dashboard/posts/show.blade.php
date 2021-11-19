c634b189581555792e18b9194fd825b1f4b8b0ad
Hanif437 <Putrahanif437@gmail.com>
2021-11-19 19:40:21 +0700

    memodifikasi file show.blade.php pada video ke 4 di pertemuan ke 9

--- coba-laravel/resources/views/dashboard/posts/show.blade.php
+++ coba-laravel/resources/views/dashboard/posts/show.blade.php
@@ -11,9 +11,13 @@
 
                 <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to all My Posts</a>
 
-                <a href="" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
+                <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
 
-                <a href="" class="btn btn-danger"><span data-feather="x-circle"></span> Delete</a>
+                <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                    @method('delete')
+                    @csrf
+                    <button class="btn btn-danger" onclick="return confirm('Are you sure ?')"><span data-feather="x-circle"></span> Delete</button>
+                  </form>
                 
                 <img src="https://source.unsplash.com/800x400/?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="image-fluid mt-3">
                 <article class="my-3 fs-5">
